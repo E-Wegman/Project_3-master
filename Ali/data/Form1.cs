b724c63fc20394f3d42602c468686a32594217e4
AliAlFatly <36470971+AliAlFatly@users.noreply.github.com>
2018-03-21 12:41:04 +0100

    diagram concept

---
+++ Ali/data/Form1.cs
@@ -0,0 +1,45 @@
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace data
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (StraatrovenEntities db = new StraatrovenEntities())
+            {
+                var data = db.wz15();
+                ColumnSeries col = new ColumnSeries() { DataLabels = true, Values = new ChartValues<int>(), LabelPoint = point => point.Y.ToString() };
+                Axis ax = new Axis() { Separator = new Separator() { Step = 1, IsEnabled = false } };
+                ax.Labels = new List<string>();
+                foreach(var x in data)
+                {
+                    col.Values.Add(x.wz);
+                    ax.Labels.Add(x.bn);
+                }
+                cartesianChart1.Series.Add(col);
+                cartesianChart1.AxisX.Add(ax);
+                cartesianChart1.AxisY.Add(new Axis
+                {
+                    LabelFormatter = value => value.ToString(),
+                    Separator = new Separator() 
+                });
+            }
+        }
+    }
+}
